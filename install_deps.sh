b2ff3c6b6541b0561e542aa5334b06e78cd3d318
gabriel rivera <rivera@infocortex.com>
2014-10-09 00:25:10 -0700

    fixed dependencies

--- install_deps.sh
+++ install_deps.sh
@@ -4,4 +4,5 @@ puppet module install puppetlabs/stdlib
 puppet module install puppetlabs-apache
 puppet module install jfryman/nginx
 
+git clone https://github.com/Adcade/puppet-uwsgi.git /etc/puppet/modules/uwsgi
 git clone git://github.com/stankevich/puppet-python.git /etc/puppet/modules/python
